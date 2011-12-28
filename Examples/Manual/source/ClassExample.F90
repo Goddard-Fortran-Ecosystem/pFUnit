79b561fe1836de6c493207665fcec4f8e13ce4b4
James Matykiewicz <james@iter.txcorp.com>
2011-12-27 17:47:28 -0700

    add source for test

--- Examples/Manual/source/ClassExample.F90
+++ Examples/Manual/source/ClassExample.F90
@@ -1,27 +1,27 @@
 #include 'reflection.h'
-module ExampleTestCase_mod
+module ClassExample_mod
    use TestCase_mod, only: TestCase
    implicit none
    private
 
    public :: suite
-   public :: newExampleTestCase
-   public :: ExampleTestCase
+   public :: newClassExample
+   public :: ClassExample
    public :: method1, method2
    public :: methodWith2Exceptions
 
-   type, extends(TestCase) :: ExampleTestCase
+   type, extends(TestCase) :: ClassExample
       character(len=20), public :: runLog
       procedure(method1), pointer :: testMethod => null()
    contains
       procedure :: runTestMethod
-   end type ExampleTestCase
+   end type ClassExample
 
    abstract interface
       subroutine I_method(this)
         use Test_mod
-        import ExampleTestCase
-        class (ExampleTestCase), intent(inOut) :: this
+        import ClassExample
+        class (ClassExample), intent(inOut) :: this
       end subroutine I_method
    end interface
 
@@ -31,9 +31,9 @@ contains
      use TestSuite_mod, only: TestSuite, newTestSuite
       type (TestSuite), pointer :: suite
 
-      suite => newTestSuite('ExampleTestCase')
+      suite => newTestSuite('ClassExample')
 
-#define ADD(method) call suite%addTest(newExampleTestCase(REFLECT(method)))
+#define ADD(method) call suite%addTest(newClassExample(REFLECT(method)))
 
       ADD(method1)
       ADD(method2)
@@ -41,8 +41,8 @@ contains
       
    end function suite
 
-   function newExampleTestCase(method, name) result(this)
-      type(ExampleTestCase), pointer :: this
+   function newClassExample(method, name) result(this)
+      type(ClassExample), pointer :: this
       procedure(I_method) :: method
       character(len=*), intent(in) :: name
 
@@ -50,35 +50,35 @@ contains
       this%testMethod => method
       call this%setName(name)
 
-    end function newExampleTestCase
+    end function newClassExample
 
    recursive subroutine runTestMethod(this)
-      class(ExampleTestCase), intent(inOut) :: this
+      class(ClassExample), intent(inOut) :: this
       call this%testMethod()
    end subroutine runTestMethod
 
    subroutine method1(this)
-      class (ExampleTestCase), intent(inOut) :: this
+      class (ClassExample), intent(inOut) :: this
       this%runLog = 'run method1'
    end subroutine method1
 
    subroutine method2(this)
-      class (ExampleTestCase), intent(inOut) :: this
+      class (ClassExample), intent(inOut) :: this
       this%runLog = 'run method2'
    end subroutine method2
 
    subroutine methodWith2Exceptions(this)
       use Exception_mod, only: throw
-      class (ExampleTestCase), intent(inOut) :: this
+      class (ClassExample), intent(inOut) :: this
 
       call throw('failure A')
       call throw('failure B')
    end subroutine methodWith2Exceptions
 
    subroutine delete_(this)
-      type (ExampleTestCase), intent(inOut) :: this
+      type (ClassExample), intent(inOut) :: this
 !!$      nullify(this%testMethod)
    end subroutine delete_
 
-end module ExampleTestCase_mod
+end module ClassExample_mod
 
