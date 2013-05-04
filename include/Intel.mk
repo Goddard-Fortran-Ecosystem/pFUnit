a9f0a2930a05f6c95b39f2379080c60a9ca0ab75
Tom <Thomas.L.Clune@nasa.gov>
2013-05-04 17:43:50 -0400

    Added check uninit for ifort.
    Did not identify the previous problem, but can't hurt in the long run.

--- include/Intel.mk
+++ include/Intel.mk
@@ -5,7 +5,7 @@ M=-I
 L=-L
 
 F90FLAGS += -assume realloc_lhs
-F90FLAGS += -g -O0 -traceback -check uninit -check bounds -check stack
+F90FLAGS += -g -O0 -traceback -check uninit -check bounds -check stack -check uninit
 F90FLAGS += -DSTRINGIFY_OPERATOR
 
 CPPFLAGS+=-DSTRINGIFY_OPERATOR -DIntel
