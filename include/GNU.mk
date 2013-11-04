bb92491396dab584bf14074487002d79219e9e6f
Michael Rilee <Michael.L.Rilee-1@nasa.gov>
2013-11-04 13:15:31 -0500

    Added FPPFLAGS to include/GNU.mk.  Not sure about deleting CPPFLAGS from that file.

--- include/GNU.mk
+++ include/GNU.mk
@@ -7,6 +7,9 @@ L=-L
 F90FLAGS += -g -O0 -fbacktrace
 F90FLAGS += -fbounds-check -fcheck=mem
 F90FLAGS += -DSTRINGIFY_SIMPLE
+FPPFLAGS += -DGNU
+
+# The ramifications across all GNUish configurations of eliding CPPFLAGS here are not known. MLR 2013-1104
 CPPFLAGS += -DGNU
 
 
