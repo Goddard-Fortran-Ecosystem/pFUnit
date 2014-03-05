986f371192311f29f56327c2f04eadbfe9e41799
Michael Rilee <Michael.L.Rilee-1@nasa.gov>
2014-03-05 06:29:45 -0500

    Tweaking NAG compiler options: removing -gline from OpenMP compile.

--- include/NAG.mk
+++ include/NAG.mk
@@ -4,10 +4,13 @@ I=-I
 M=-I
 L=-L
 
-F90FLAGS += -g -O0 -f2003 -w=uda -gline -fpp -mismatch_all
+# F90FLAGS += -g -O0 -f2003 -w=uda -gline -fpp -mismatch_all
+F90FLAGS += -g -O0 -f2008 -w=uda -fpp -mismatch_all
 
 ifeq ($(USEOPENMP),YES)
 F90FLAGS += -openmp
+else
+F90FLAGS += -gline
 endif
 
 F90_HAS_CPP=NO
