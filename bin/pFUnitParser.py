c907676bf0db3eb5900ad62bdb1a871d8274b63b
Tom <Thomas.L.Clune@nasa.gov>
2014-03-06 13:49:24 -0500

    Improved consistency of local variable names.

--- bin/pFUnitParser.py
+++ bin/pFUnitParser.py
@@ -415,52 +415,52 @@ class Parser():
 
         self.outputFile.write("   suite = newTestSuite('" + self.suiteName + "')\n\n")
 
-        for method in self.userTestMethods:
+        for testMethod in self.userTestMethods:
             if 'type' in self.userTestCase:
-                self.addUserTestMethod(method)
+                self.addUserTestMethod(testMethod)
             else:
-                if 'npRequests' in method:
-                    self.addMpiTestMethod(method)
+                if 'npRequests' in testMethod:
+                    self.addMpiTestMethod(testMethod)
                 else: # vanilla
-                    self.addSimpleTestMethod(method)
+                    self.addSimpleTestMethod(testMethod)
             self.outputFile.write('\n')
 
         self.outputFile.write('\nend function ' + self.suiteName + '\n\n')
 
-    def addSimpleTestMethod(self, method):
-        args = "'" + method['name'] + "', " + method['name']
-        if 'setUp' in method:
-            args += ', ' + method['setUp']
+    def addSimpleTestMethod(self, testMethod):
+        args = "'" + testMethod['name'] + "', " + testMethod['name']
+        if 'setUp' in testMethod:
+            args += ', ' + testMethod['setUp']
         elif 'setUp' in self.userTestCase:
             args += ', ' + self.userTestCase['setUp']
 
-        if 'tearDown' in method:
-            args += ', ' + method['tearDown']
+        if 'tearDown' in testMethod:
+            args += ', ' + testMethod['tearDown']
         elif 'tearDown' in self.userTestCase:
             args += ', ' + self.userTestCase['tearDown']
 
         self.outputFile.write('   call suite%addTest(newTestMethod(' + args + '))\n')
 
-    def addMpiTestMethod(self, method):
-        for npes in method['npRequests']:
-            args = "'" + method['name'] + "', " + method['name'] + ", " + str(npes)
-            if 'setUp' in method:
-                args += ', ' + method['setup'] + ', ' + method['tearDown']
+    def addMpiTestMethod(self, testMethod):
+        for npes in testMethod['npRequests']:
+            args = "'" + testMethod['name'] + "', " + testMethod['name'] + ", " + str(npes)
+            if 'setUp' in testMethod:
+                args += ', ' + testMethod['setup'] + ', ' + testMethod['tearDown']
             self.outputFile.write('   call suite%addTest(newMpiTestMethod(' + args + '))\n')
     
-    def addUserTestMethod(self, method):
+    def addUserTestMethod(self, testMethod):
 
-        args = "'" + method['name'] + "', " + method['name']
-        if 'npRequests' in method:
-            npRequests = method['npRequests']
+        args = "'" + testMethod['name'] + "', " + testMethod['name']
+        if 'npRequests' in testMethod:
+            npRequests = testMethod['npRequests']
         else:
             if 'npRequests' in self.userTestCase:
                 npRequests = self.userTestCase['npRequests']
             else:
                 npRequests = [1]
 
-        if 'cases' in method:
-            cases = method['cases']
+        if 'cases' in testMethod:
+            cases = testMethod['cases']
         elif 'cases' in self.userTestCase:
             cases = self.userTestCase['cases']
 
@@ -468,19 +468,19 @@ class Parser():
 
         if 'cases' in locals():
             testParameterArg = ', testParameter'
-            self.outputFile.write('   cases = ' + method['cases'] + '\n')
+            self.outputFile.write('   cases = ' + testMethod['cases'] + '\n')
             self.outputFile.write('   testParameters = [(' + 
                                   self.userTestCase['testParameterConstructor'] + 
                                   '(cases(iCase)), iCase = 1, size(cases))]\n\n')
 
         if 'testParameterType' in self.userTestCase:
-            if 'testParameters' in method:
-                testParameters = method['testParameters']
+            if 'testParameters' in testMethod:
+                testParameters = testMethod['testParameters']
             elif 'testParameters' in self.userTestCase:
                 testParameters = self.userTestCase['testParameters']
 
         isMpiTestCase = 'npRequests' in self.userTestCase
-        isMpiTestCase = isMpiTestCase or any('npRequests' in method for method in self.userTestMethods)
+        isMpiTestCase = isMpiTestCase or any('npRequests' in testMethod for testMethod in self.userTestMethods)
 
         if 'testParameters' in locals():
             testParameterArg = ', testParameter'
@@ -549,7 +549,7 @@ class Parser():
 
         if 'type' in self.userTestCase:
             isMpiTestCase = 'npRequests' in self.userTestCase
-            isMpiTestCase = isMpiTestCase or any('npRequests' in method for method in self.userTestMethods)
+            isMpiTestCase = isMpiTestCase or any('npRequests' in testMethod for testMethod in self.userTestMethods)
             if isMpiTestCase and not 'testParameterType' in self.userTestCase:
                 self.userTestCase['testParameterType'] = 'MpiTestParameter'
 
