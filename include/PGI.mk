88ab4a0e9179f005a68ca4e8faee83dbd381fae1
Michael Rilee <Michael.L.Rilee-1@nasa.gov>
2015-02-16 10:56:58 -0500

    Added OpenMP flag to PGI.mk.  This fixes why PGI was getting 198 tests instead of 200 when MPI and OpenMPI were both enabled.

--- include/PGI.mk
+++ include/PGI.mk
@@ -6,22 +6,9 @@ L=-L
 
 FFLAGS += -O0 -g -traceback -Mallocatable=03 -Mbounds -Mchkfpstk -Mchkstk
 
-# -O0 -g -traceback -Mchkfpstk -Mchkstk
-# -Mallocatable=03
-# FFLAGS += 
-# Segmentation fault: 11
-
-# FFLAGS += -Mbounds
-# Segmentation fault: 11
-
-# FFLAGS += -O0 -g -traceback -Mchkfpstk -Mchkstk -Mallocatable=03
-# Segmentation fault: 11
-
-# FFLAGS += -O0 -g -traceback -Mbounds -Mchkfpstk -Mchkstk -Mallocatable=03
-#0: Subscript out of range for array this%tests (TestSuite.F90: 287)
-#    subscript=8496367, lower bound=1, upper bound=2, dimension=1
-
-# FFLAGS += -Mbounds -Minfo=all -traceback -Mchkfpstk -Mchkstk -Mdalign -Mdclchk -Mdepchk -Miomutex -Mrecursive -Msave -Ktrap=fp -O0 -g -byteswapio -Mallocatable=03 
+ifeq ($(USEOPENMP),YES)
+FFLAGS += -mp
+endif
 
 FPPFLAGS += -DPGI
 CPPFLAGS += -DPGI -Mpreprocess
