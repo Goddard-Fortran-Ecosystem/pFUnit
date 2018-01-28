512a1be58f6776b0c6502e1bbe34795d59a40659
Tom Clune <tom.clune@nasa.gov>
2018-01-28 16:54:54 -0500

    A bit of cleanup.

--- cmake/CheckFortranSource.cmake
+++ cmake/CheckFortranSource.cmake
@@ -1,17 +1,5 @@
-macro (CHECK_FORTRAN_SOURCE_COMPILE file var)
+macro (CHECK_FORTRAN_SOURCE_RUN file var)
 
-  if (NOT CMAKE_REQUIRED_QUIET)
-    message (STATUS "Performing Test ${var}")
-  endif ()
-
-  if (${ARGN})
-  try_run (
-    ${var}
-    ${CMAKE_BINARY_DIR}
-    ${file}
-    CMAKE_FLAGS "-DCOMPILE_DEFINITIONS=${CMAKE_REQUIRED_DEFINITIONS}"
-    )
-else ()
   try_run (
     run compile
     ${CMAKE_BINARY_DIR}
@@ -19,58 +7,41 @@ else ()
     CMAKE_FLAGS "-DCOMPILE_DEFINITIONS=${CMAKE_REQUIRED_DEFINITIONS}"
     RUN_OUTPUT_VARIABLE ${var}
     )
-endif ()
 
-if (${var})
-  string(STRIP ${${var}} ${var})
+  if (${var})
+    string(STRIP ${${var}} ${var})
     if (NOT CMAKE_REQUIRED_QUIET)
-      message(STATUS "Performing Test ${var}: SUCCESS ${${var}}")
+      message(STATUS "Performing Test ${var}: SUCCESS (value=${${var}})")
     endif ()
-
+    
     add_definitions(-D${var}=${${var}})
-
+    
   else ()
-
+    
     if (NOT CMAKE_REQUIRED_QUIET)
       message(STATUS "Performing Test ${var}: FAILURE")
     endif ()
-
+    
   endif ()
 
-endmacro (CHECK_FORTRAN_SOURCE_COMPILE)
+endmacro (CHECK_FORTRAN_SOURCE_RUN)
 
 
-macro (CHECK_FORTRAN_SOURCE_RUN file var)
+macro (CHECK_FORTRAN_SOURCE_COMPILE file var)
 
-  if (NOT CMAKE_REQUIRED_QUIET)
-    message (STATUS "Performing Test ${var}")
-  endif ()
-
-  try_run (
-    code_runs
+  try_compile (
     code_compiles
     ${CMAKE_BINARY_DIR}
     ${file}
+    CMAKE_FLAGS "-DCOMPILE_DEFINITIONS=${CMAKE_REQUIRED_DEFINITIONS}"
     )
 
   if (${code_compiles})
-    if (${code_runs} EQUAL 0)
-
-      if (NOT CMAKE_REQUIRED_QUIET)
-	message (STATUS "Performing Test ${var}: SUCCESS")
-      endif ()
-
-      add_definitions(-D${var})
-
-      set (${var} 1)
-
-    else ()
-
-      if (NOT CMAKE_REQUIRED_QUIET)
-	message (STATUS "Performing Test ${var}: RUN FAILURE")
-      endif ()
-
+    if (NOT CMAKE_REQUIRED_QUIET)
+      message (STATUS "Performing Test ${var}: SUCCESS")
     endif ()
+
+    add_definitions(-D${var})
 
   else ()
 
@@ -80,4 +51,4 @@ macro (CHECK_FORTRAN_SOURCE_RUN file var)
 
   endif()
 
-endmacro (CHECK_FORTRAN_SOURCE_RUN)
+endmacro (CHECK_FORTRAN_SOURCE_COMPILE)
