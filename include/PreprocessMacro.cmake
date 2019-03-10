326da75824e551bb8e6ffdac3daecf16ec33ccd9
Tom Clune <tom.clune@nasa.gov>
2019-03-10 10:49:41 -0400

    Now use single macro for preprocessing pfunit tests sources.
    Note that this macro is inconsistent with the one in the cmake config
    file for external projects and additional thought is needed.

--- include/PreprocessMacro.cmake
+++ include/PreprocessMacro.cmake
@@ -1,43 +1,46 @@
-set (PFUNIT_PREPROCESSOR python $ENV{PFUNIT}/bin/pFUnitParser.py)
-set (PFUNIT_SUFFIX "\\.pfunit")
+set (PFUNIT_PREPROCESSOR python ${PFUNIT_SOURCE_DIR}/bin/pFUnitParser.py)
+
 # - Pass a list of files through the pFUnit macro processor
 #
-# ADD_PFUNIT_SOURCES( OUTVAR source1 ... sourceN )
+# ADD_PFUNIT_SOURCES( out_var source1 ... sourceN )
 #
-#    OUTVAR    A list containing all the output file names, suitable
+#    out_var    A list containing all the output file names, suitable
 #                    to be passed to add_executable or add_library.
 #
-# If the source files have a .m4 suffix it is stripped from the output
+# The suffix of the source files are stripped from the output
 # file name. The output files are placed in the same relative location
 # to CMAKE_CURRENT_BINARY_DIR as they are to CMAKE_CURRENT_SOURCE_DIR.
 #
 # Example:
 #    add_pfunit_sources( SRCS src/test1.cxx.pfunit src/test2.cxx.pfunit )
 #    add_executable( test ${SRCS} )
-function( ADD_PFUNIT_SOURCES OUTVAR )
-     set( outfiles )
-     foreach( f ${ARGN} )
+function( ADD_PFUNIT_SOURCES out_var )
+     set( out_files )
+     foreach( file ${ARGN} )
          # first we might need to make the input file absolute
-         get_filename_component( f "${f}" ABSOLUTE )
+         get_filename_component( abs_file "${file}" ABSOLUTE )
          # get the relative path of the file to the current source dir
-         file( RELATIVE_PATH baseFile "${CMAKE_CURRENT_SOURCE_DIR}" "${f}" )
-         # strip the .pfunit off the end if present
-         string( REGEX REPLACE "${PFUNIT_SUFFIX}" ".F90" outFile "${CMAKE_CURRENT_BINARY_DIR}/${baseFile}" )
+         file( RELATIVE_PATH rel_file "${CMAKE_CURRENT_SOURCE_DIR}" "${abs_file}" )
+
+         # replace the extension with .F90 to determine the output file name
+	 get_filename_component (extension ${file} EXT)
+         string( REGEX REPLACE "${extension}" ".F90" out_file "${CMAKE_CURRENT_BINARY_DIR}/${rel_file}" )
          # append the output file to the list of outputs
-         list( APPEND outfiles "${outFile}" )
+         list( APPEND out_files "${out_file}" )
          # create the output directory if it doesn't exist
-         get_filename_component( dir "${outFile}" PATH )
+         get_filename_component( dir "${out_file}" PATH )
          if( NOT IS_DIRECTORY "${dir}" )
              file( MAKE_DIRECTORY "${dir}" )
          endif( NOT IS_DIRECTORY "${dir}" )
          # now add the custom command to generate the output file
-         add_custom_command( OUTPUT "${outFile}"
-             COMMAND ${PFUNIT_PREPROCESSOR} "${f}" "${outFile}"
-             DEPENDS "${f}"
+         add_custom_command( OUTPUT "${out_file}"
+             COMMAND ${PFUNIT_PREPROCESSOR} "${abs_file}" "${out_file}"
+             DEPENDS "${abs_file}"
+	     WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
              )
-     endforeach( f )
+     endforeach ( )
      # set the output list in the calling scope
-     set( ${OUTVAR} ${outfiles} PARENT_SCOPE )
+     set( ${out_var} ${${out_var}} ${out_files} PARENT_SCOPE )
 endfunction( ADD_PFUNIT_SOURCES )
 
 
