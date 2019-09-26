8e420934e0581734c80173d0fe78072b2a64dd33
Mehdi Chinoune <chinoune.mehdi@gmail.com>
2019-09-26 08:09:05 +0100

    Fix some issues with Python3

--- bin/mods/pre/__init__.py
+++ bin/mods/pre/__init__.py
@@ -3,5 +3,5 @@
 import sys
 
 if sys.version < '2.7':
-   print 'This module requires at least version 2.7 of python'
+   print ('This module requires at least version 2.7 of python')
    sys.exit(1)
