45ee318acf23573699d16e829ad248c552f2b653
Tom Clune <thomas.l.clune@nasa.gov>
2019-11-08 15:49:01 -0500

    Updates for release v4.1.0

--- src/funit/core/posix_predefined.c
+++ src/funit/core/posix_predefined.c
@@ -1,3 +1,8 @@
+#ifndef __OSX_AVAILABLE_STARTING
+#  define __OSX_AVAILABLE_STARTING(_osx, ios)
+#  define __OSX_AVAILABLE_BUT_DEPRECATED(_osxIntro, osxDep, iosIntro, iosDep)
+#  define __OSX_AVAILABLE_BUT_DEPRECATED_MSG(_osxIntro, osxDep, iosIntro, iosDep, _msg)
+#endif
 #include<stdlib.h>
 #include<errno.h>
 #include<stdio.h>
