fe6457e81bf0ecc9f84d4f4b2aafb3e480db1437
William Jamieson <william.b.jamieson@nasa.gov>
2020-08-21 22:52:17 -0400

    Finished abstraction of relational matchers

--- src/funit/fhamcrest/IsGreater.F90
+++ src/funit/fhamcrest/IsGreater.F90
@@ -9,7 +9,11 @@ module pf_IsGreater
 
    type, extends(RelationalMatcher) :: IsGreater
    contains
-      procedure :: matches
+      procedure :: relation_int32
+      procedure :: relation_int64
+      procedure :: relation_real32
+      procedure :: relation_real64
+      procedure :: relation_real128
    end type IsGreater
 
 contains
@@ -20,48 +24,43 @@ contains
       call matcher%super(operand, "strictly greater than ", " is less than or equal to ")
    end function greater_than
 
-   recursive logical function matches(this, actual_value)
+   recursive logical function relation_int32(this, expected, actual)
       class(IsGreater), intent(in) :: this
-      class(*), intent(in) :: actual_value
+      integer(kind=INT32), intent(in) :: expected
+      integer(kind=INT32), intent(in) :: actual
 
-      select type(e => this%expected_value)
-      type is (integer(kind=INT32))
-         select type(a => actual_value)
-         type is (integer(kind=INT32))
-            matches = (a > e)
-         class default
-            matches = .false.
-         end select
-      type is (integer(kind=INT64))
-         select type(a => actual_value)
-         type is (integer(kind=INT64))
-            matches = (a > e)
-         class default
-            matches = .false.
-         end select
-      type is (real(kind=REAL32))
-         select type(a => actual_value)
-         type is (real(kind=REAL32))
-            matches = (a > e)
-         class default
-            matches = .false.
-         end select
-      type is (real(kind=REAL64))
-         select type(a => actual_value)
-         type is (real(kind=REAL64))
-            matches = (a > e)
-         class default
-            matches = .false.
-         end select
-      type is (real(kind=REAL128))
-         select type(a => actual_value)
-         type is (real(kind=REAL128))
-            matches = (a > e)
-         class default
-            matches = .false.
-         end select
-      class default
-         matches = .false.
-      end select
-   end function matches
+      relation_int32 = (actual > expected)
+   end function relation_int32
+
+   recursive logical function relation_int64(this, expected, actual)
+      class(IsGreater), intent(in) :: this
+      integer(kind=INT64), intent(in) :: expected
+      integer(kind=INT64), intent(in) :: actual
+
+      relation_int64 = (actual > expected)
+   end function relation_int64
+
+   recursive logical function relation_real32(this, expected, actual)
+      class(IsGreater), intent(in) :: this
+      real(kind=REAL32), intent(in) :: expected
+      real(kind=REAL32), intent(in) :: actual
+
+      relation_real32 = (actual > expected)
+   end function relation_real32
+
+   recursive logical function relation_real64(this, expected, actual)
+      class(IsGreater), intent(in) :: this
+      real(kind=REAL64), intent(in) :: expected
+      real(kind=REAL64), intent(in) :: actual
+
+      relation_real64 = (actual > expected)
+   end function relation_real64
+
+   recursive logical function relation_real128(this, expected, actual)
+      class(IsGreater), intent(in) :: this
+      real(kind=REAL128), intent(in) :: expected
+      real(kind=REAL128), intent(in) :: actual
+
+      relation_real128 = (actual > expected)
+   end function relation_real128
 end module pf_IsGreater
--- src/funit/fhamcrest/RelationalMatcher.F90
+++ src/funit/fhamcrest/RelationalMatcher.F90
@@ -18,7 +18,62 @@ module pf_RelationalMatcher
       procedure :: describe_to
       procedure :: describe_mismatch
       procedure :: describe_numeric_mismatch
+
+      procedure :: matches
+
+      procedure(i_relation_int32), deferred :: relation_int32
+      procedure(i_relation_int64), deferred :: relation_int64
+      procedure(i_relation_real32), deferred :: relation_real32
+      procedure(i_relation_real64), deferred :: relation_real64
+      procedure(i_relation_real128), deferred :: relation_real128
    end type RelationalMatcher
+
+   abstract interface
+      recursive logical function i_relation_int32(this, expected, actual)
+         use, intrinsic :: iso_fortran_env, only: INT32
+         import RelationalMatcher
+
+         class(RelationalMatcher), intent(in) :: this
+         integer(kind=INT32), intent(in) :: expected
+         integer(kind=INT32), intent(in) :: actual
+      end function i_relation_int32
+
+      recursive logical function i_relation_int64(this, expected, actual)
+         use, intrinsic :: iso_fortran_env, only: INT64
+         import RelationalMatcher
+
+         class(RelationalMatcher), intent(in) :: this
+         integer(kind=INT64), intent(in) :: expected
+         integer(kind=INT64), intent(in) :: actual
+      end function i_relation_int64
+
+      recursive logical function i_relation_real32(this, expected, actual)
+         use, intrinsic :: iso_fortran_env, only: REAL32
+         import RelationalMatcher
+
+         class(RelationalMatcher), intent(in) :: this
+         real(kind=REAL32), intent(in) :: expected
+         real(kind=REAL32), intent(in) :: actual
+      end function i_relation_real32
+
+      recursive logical function i_relation_real64(this, expected, actual)
+         use, intrinsic :: iso_fortran_env, only: REAL64
+         import RelationalMatcher
+
+         class(RelationalMatcher), intent(in) :: this
+         real(kind=REAL64), intent(in) :: expected
+         real(kind=REAL64), intent(in) :: actual
+      end function i_relation_real64
+
+      recursive logical function i_relation_real128(this, expected, actual)
+         use, intrinsic :: iso_fortran_env, only: REAL128
+         import RelationalMatcher
+
+         class(RelationalMatcher), intent(in) :: this
+         real(kind=REAL128), intent(in) :: expected
+         real(kind=REAL128), intent(in) :: actual
+      end function i_relation_real128
+   end interface
 
 contains
    subroutine super(this, expected_value, relationship, err_msg)
@@ -70,4 +125,51 @@ contains
       call description%append_text(this%err_msg)
       call description%append_value(this%expected_value)
    end subroutine describe_numeric_mismatch
+
+   recursive logical function matches(this, actual_value)
+      class(RelationalMatcher), intent(in) :: this
+      class(*), intent(in) :: actual_value
+
+      logical :: result
+
+      select type(e => this%expected_value)
+      type is (integer(kind=INT32))
+         select type(a => actual_value)
+         type is (integer(kind=INT32))
+            matches = this%relation_int32(e, a)
+         class default
+            matches = .false.
+         end select
+      type is (integer(kind=INT64))
+         select type(a => actual_value)
+         type is (integer(kind=INT64))
+            matches = this%relation_int64(e, a)
+         class default
+            matches = .false.
+         end select
+      type is (real(kind=REAL32))
+         select type(a => actual_value)
+         type is (real(kind=REAL32))
+            matches = this%relation_real32(e, a)
+         class default
+            matches = .false.
+         end select
+      type is (real(kind=REAL64))
+         select type(a => actual_value)
+         type is (real(kind=REAL64))
+            matches = this%relation_real64(e, a)
+         class default
+            matches = .false.
+         end select
+      type is (real(kind=REAL128))
+         select type(a => actual_value)
+         type is (real(kind=REAL128))
+            matches = this%relation_real128(e, a)
+         class default
+            matches = .false.
+         end select
+      class default
+         matches = .false.
+      end select
+   end function matches
 end module pf_RelationalMatcher
