b1a73ce54f2edc5a93de6ffb1757033934149f9c
Michael Andree Heuer <heuer@pc.rwth-aachen.de>
2020-11-19 16:59:59 +0100

    Use project source dir.

--- cmake/CheckCompilerCapabilities.cmake
+++ cmake/CheckCompilerCapabilities.cmake
@@ -1,12 +1,12 @@
-include (${PFUNIT_SOURCE_DIR}/cmake/CheckFortranSource.cmake)
+include (${PROJECT_SOURCE_DIR}/cmake/CheckFortranSource.cmake)
 
 CHECK_FORTRAN_SOURCE_RUN (
-  ${PFUNIT_SOURCE_DIR}/cmake/Trial_sources/LOGICAL_DEFAULT_KIND.F90
+  ${PROJECT_SOURCE_DIR}/cmake/Trial_sources/LOGICAL_DEFAULT_KIND.F90
   _LOGICAL_DEFAULT_KIND
   )
 
 CHECK_FORTRAN_SOURCE_RUN (
-  ${PFUNIT_SOURCE_DIR}/cmake/Trial_sources/INT_DEFAULT_KIND.F90
+  ${PROJECT_SOURCE_DIR}/cmake/Trial_sources/INT_DEFAULT_KIND.F90
   _INT_DEFAULT_KIND
   )
 foreach (kind 8 16 32 64)
@@ -14,19 +14,19 @@ foreach (kind 8 16 32 64)
   set(CMAKE_REQUIRED_DEFINITIONS -D_KIND=INT${kind})
 
   CHECK_FORTRAN_SOURCE_RUN (
-    ${PFUNIT_SOURCE_DIR}/cmake/Trial_sources/INT_KIND.F90
+    ${PROJECT_SOURCE_DIR}/cmake/Trial_sources/INT_KIND.F90
     _ISO_INT${kind}
     )
 
 endforeach()
 
 CHECK_FORTRAN_SOURCE_RUN (
-  ${PFUNIT_SOURCE_DIR}/cmake/Trial_sources/REAL_DEFAULT_KIND.F90
+  ${PROJECT_SOURCE_DIR}/cmake/Trial_sources/REAL_DEFAULT_KIND.F90
   _REAL_DEFAULT_KIND
   )
 
 CHECK_FORTRAN_SOURCE_RUN (
-  ${PFUNIT_SOURCE_DIR}/cmake/Trial_sources/DOUBLE_DEFAULT_KIND.F90
+  ${PROJECT_SOURCE_DIR}/cmake/Trial_sources/DOUBLE_DEFAULT_KIND.F90
   _DOUBLE_DEFAULT_KIND
   )
 
@@ -37,12 +37,12 @@ foreach (kind 32 64 80 128 256)
   try_compile (
     code_compiles
     ${CMAKE_BINARY_DIR}
-    ${PFUNIT_SOURCE_DIR}/cmake/Trial_sources/REAL_KIND.F90
+    ${PROJECT_SOURCE_DIR}/cmake/Trial_sources/REAL_KIND.F90
     CMAKE_FLAGS "-DCOMPILE_DEFINITIONS=${CMAKE_REQUIRED_DEFINITIONS}")
   
   if (code_compiles)
     CHECK_FORTRAN_SOURCE_RUN (
-      ${PFUNIT_SOURCE_DIR}/cmake/Trial_sources/REAL_KIND.F90
+      ${PROJECT_SOURCE_DIR}/cmake/Trial_sources/REAL_KIND.F90
       _ISO_REAL${kind}
       )
   endif ()
@@ -50,12 +50,12 @@ foreach (kind 32 64 80 128 256)
   try_compile (
     code_compiles
     ${CMAKE_BINARY_DIR}
-    ${PFUNIT_SOURCE_DIR}/cmake/Trial_sources/REAL_KIND.F90
+    ${PROJECT_SOURCE_DIR}/cmake/Trial_sources/REAL_KIND.F90
     CMAKE_FLAGS "-DCOMPILE_DEFINITIONS=${CMAKE_REQUIRED_DEFINITIONS}")
   
   if (code_compiles)
     CHECK_FORTRAN_SOURCE_RUN(
-      ${PFUNIT_SOURCE_DIR}/cmake/Trial_sources/REAL_KIND_IEEE_SUPPORT.F90
+      ${PROJECT_SOURCE_DIR}/cmake/Trial_sources/REAL_KIND_IEEE_SUPPORT.F90
       _REAL${kind}_IEEE_SUPPORT
       )
   endif ()
