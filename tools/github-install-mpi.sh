e42a38755104677ab7e6292f8e24eff66c1a1e52
Matthew Thompson <matthew.thompson@nasa.gov>
2020-12-01 10:36:27 -0500

    Try brew in mpi installation

---
+++ tools/github-install-mpi.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+# source: mpi4py
+# https://github.com/mpi4py/mpi4py/blob/master/conf/travis/install-mpi.sh
+
+set -e
+
+MPI_IMPL="$1"
+MPI_VER="$2"
+os=`uname`
+
+if [ ! -d "${HOME}/local/${MPI_IMPL}/bin" ]
+then
+   # https://github.com/Homebrew/legacy-homebrew/issues/19046
+   if [ "${os}" == "Darwin" ]
+   then
+      brew reinstall --build-from-source gmp mpfr
+   fi
+
+   mkdir -p ${HOME}/MPI/src/openmpi && cd ${HOME}/MPI/src/openmpi
+   wget --no-check-certificate https://download.open-mpi.org/release/open-mpi/v${MPI_VER%.*}/${MPI_IMPL}-${MPI_VER}.tar.bz2
+   tar xjf ${MPI_IMPL}-${MPI_VER}.tar.bz2 && rm ${MPI_IMPL}-${MPI_VER}.tar.bz2
+   cd ${MPI_IMPL}-${MPI_VER}
+   ./configure --prefix=${HOME}/local/${MPI_IMPL} --disable-wrapper-rpath --disable-wrapper-runpath --with-hwloc=internal --with-libevent=internal
+   make -j $(nproc)
+   make install-strip
+   cd .. && rm -r ${MPI_IMPL}-${MPI_VER}
+   exit 0
+else
+   echo "Using cached ${MPI_IMPL} directory";
+   exit 0
+fi
