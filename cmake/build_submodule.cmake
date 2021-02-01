f1dc079d27611da35b116527df349af126770a3d
Peter Hill <zed.three@gmail.com>
2021-02-01 15:50:15 +0000

    CMake: Fix build_submodule for use in other projects
    If either the project or target are found, then don't use the
    submodule. This should prevent issues with duplicate targets

--- cmake/build_submodule.cmake
+++ cmake/build_submodule.cmake
@@ -9,26 +9,29 @@ macro (build_submodule submodule)
 
   find_package(${build_submodule_PROJECT} QUIET)
 
-  if (NOT ${project}_FOUND)
-    if (NOT TARGET ${build_submodule_TARGET})
-
+  if (${build_submodule_PROJECT}_FOUND)
+    message(STATUS "Using package '${build_submodule_PROJECT}'")
+  else()
+    if (TARGET ${build_submodule_TARGET})
+      message(STATUS "Using target '${build_submodule_TARGET}'")
+    else()
       # from https://cliutils.gitlab.io/modern-cmake/chapters/projects/submodule.html
       find_package(Git QUIET)
       if (GIT_FOUND AND EXISTS "${PROJECT_SOURCE_DIR}/.git")
-	option (GIT_SUBMODULE "Check submodules during build" ON)
-	if (GIT_SUBMODULE)
-	  execute_process (
-	    COMMAND ${GIT_EXECUTABLE} submodule update --init --recursive
-	    WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-	    RESULT_VARIABLE GIT_SUBMODULE_RESULT
-	    )
-	  if (NOT GIT_SUBMODULE_RESULT EQUAL "0")
-	    message(FATAL_ERROR "git submodule update --init failed with ${GIT_SUBMODULE_RESULT}, please checkout submodules")
-	  endif ()
-	endif ()
+        option (GIT_SUBMODULE "Check submodules during build" ON)
+        if (GIT_SUBMODULE)
+          execute_process (
+            COMMAND ${GIT_EXECUTABLE} submodule update --init --recursive
+            WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+            RESULT_VARIABLE GIT_SUBMODULE_RESULT
+            )
+          if (NOT GIT_SUBMODULE_RESULT EQUAL "0")
+            message(FATAL_ERROR "git submodule update --init failed with ${GIT_SUBMODULE_RESULT}, please checkout submodules")
+          endif ()
+        endif ()
       endif ()
-      add_subdirectory(${submodule})
+      message(STATUS "Using submodule ${submodule}")
+      add_subdirectory(${submodule} ${submodule})
     endif ()
   endif()
-
 endmacro()
