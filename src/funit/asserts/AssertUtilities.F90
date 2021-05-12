e2b27e79ca54224a67b551c6b36af79f77d972a4
Kurt Sansom <kurt.sansom@cfdrc.com>
2021-05-12 15:52:57 -0500

    fix: remove error handling for expected value

--- src/funit/asserts/AssertUtilities.F90
+++ src/funit/asserts/AssertUtilities.F90
@@ -359,7 +359,7 @@ contains
 
       _UNUSED_DUMMY(unused)
 
-      fail_message = base_message('assertRelMinEqual', message, index)
+      fail_message = base_message('AssertRelMinEqual', message, index)
       fail_message = fail_message // new_line('A')    // '           Expected: <' // expected // '>' 
       fail_message = fail_message // new_line('A')    // '             Actual: <' // actual // '>'
       fail_message = fail_message // new_line('A')    // '    Rel. difference: ' // difference
