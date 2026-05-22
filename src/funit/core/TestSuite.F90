184fdcad41af4bcf1b03d824d62042991e92f38b
Dominic Orchard <dom.orchard@gmail.com>
2026-05-22 12:53:40 +0100

    flang bug workaround

--- src/funit/core/TestSuite.F90
+++ src/funit/core/TestSuite.F90
@@ -79,8 +79,18 @@ contains
       class (TestSuite), intent(out) :: this
       type (TestSuite), intent(in) :: b
 
+      class (Test), pointer :: t
+      integer :: i
+
       this%name = b%name
-      this%tests = b%tests
+      ! Workaround for Flang bug: intrinsic assignment of TestVector loses nested
+      ! polymorphic allocatable components (e.g. testParameter) at depth >= 3.
+      ! Use explicit push_back instead, which uses allocate(item, source=t) per
+      ! element and correctly preserves all allocatable components.
+      do i = 1, b%tests%size()
+         t => b%tests%at(i)
+         call this%tests%push_back(t)
+      end do
       this%shuffle_enabled = b%shuffle_enabled
       this%shuffle_seed = b%shuffle_seed
 
